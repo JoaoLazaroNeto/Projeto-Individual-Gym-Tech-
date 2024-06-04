290e14ae31b0c068cb5612c15ccbb40ed99e8e6a
JoaoLazaroNeto <joao.lazaro@sptech.school.com>
2024-06-03 23:44:26 -0300

    Dashboard funcionando com gráfico dos treinos favoritos + css

--- web-data-viz/src/models/medidaModel.js
+++ web-data-viz/src/models/medidaModel.js
@@ -1,35 +1,36 @@
 var database = require("../database/config");
 
-function buscarUltimasMedidas(idAquario, limite_linhas) {
+function buscarUltimasMedidas() {
 
-    var instrucaoSql = `SELECT 
-        dht11_temperatura as temperatura, 
-        dht11_umidade as umidade,
-                        momento,
-                        DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico
-                    FROM medida
-                    WHERE fk_aquario = ${idAquario}
-                    ORDER BY id DESC LIMIT ${limite_linhas}`;
+    var instrucaoSql = `SELECT t.nome, COUNT(u.id) AS quantidade
+    FROM treino t
+    JOIN usuario u ON t.id = u.fk_treino
+    GROUP BY t.nome
+    ORDER BY quantidade DESC`;
 
-    console.log("Executando a instrução SQL: \n" + instrucaoSql);
-    return database.executar(instrucaoSql);
-}
-
-function buscarMedidasEmTempoReal(idAquario) {
-
-    var instrucaoSql = `SELECT 
-        dht11_temperatura as temperatura, 
-        dht11_umidade as umidade,
-                        DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico, 
-                        fk_aquario 
-                        FROM medida WHERE fk_aquario = ${idAquario} 
-                    ORDER BY id DESC LIMIT 1`;
+    `select idade
+     from usuario
+      order by idade desc`;
 
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }
+
+// function buscarMedidasEmTempoReal(idAquario) {
+
+//     var instrucaoSql = `SELECT 
+//         dht11_temperatura as temperatura, 
+//         dht11_umidade as umidade,
+//                         DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico, 
+//                         fk_aquario 
+//                         FROM medida WHERE fk_aquario = ${idAquario} 
+//                     ORDER BY id DESC LIMIT 1`;
+
+//     console.log("Executando a instrução SQL: \n" + instrucaoSql);
+//     return database.executar(instrucaoSql);
+// }
 
 module.exports = {
     buscarUltimasMedidas,
-    buscarMedidasEmTempoReal
+    // buscarMedidasEmTempoReal
 }
